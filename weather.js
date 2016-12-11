7207a0beed00e4fdca47269aa7636b8d1b1c14a2
kkazmierczak <krzysztof@mobilestudio.pl>
2016-12-11 15:59:00 +0100

    extension added

---
+++ weather.js
@@ -0,0 +1,35 @@
+﻿new (function() {
+    var ext = this;
+
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.get_temp = function(location, callback) {
+        // Make an AJAX call to the Open Weather Maps API
+        $.ajax({
+              url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&appid=70d186dc7ae27f1c9a580bd56b9b0a33&lang=pl&units=metric',
+              dataType: 'jsonp',
+              success: function( weather_data ) {
+                  // Got the data - parse it and return the temperature
+                  temperature = weather_data['main']['temp'];
+                  callback(temperature);
+              }
+        });
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['R', 'Temperatura w miescie %s', 'get_temp', 'Wroclaw'],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Weather extension', descriptor, ext);
+})();
